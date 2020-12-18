c5f71bdfd9aabfdb3f8b255132dfda883ab84b40
Peter <48802254+Peter2469@users.noreply.github.com>
2020-12-18 01:44:12 +0000

    Updated Main File

--- recursivedelete.py
+++ recursivedelete.py
@@ -2,8 +2,8 @@ import shutil
 
 numbernow = startingnumber
 
-name = input("What is the file you want to delete?\n")
-amount = input("\nHow many files with the same name but differentiated by numbers exist?\n")
+name = input("What is the folders/files name you want to delete?\n")
+amount = input("\nHow many folders/files with the same name but differentiated by numbers exist?\n")
 startingnumber = input("\nWhat is the first number?\n")
 
 
